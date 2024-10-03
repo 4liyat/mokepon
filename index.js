e1d7105c5dc7cf42dcd0ac9f1543b2c633dd94bd
4liyat <d_unter@hotmail.com>
2024-10-03 14:50:08 -0600

    78 se hacen cambios en el backend para saber la posicion del mokepon

--- index.js
+++ index.js
@@ -15,6 +15,11 @@ class Jugador {
     asignarMokepon(mokepon) {
         this.mokepon = mokepon
     }
+
+    actualizarPosicion(x,y) {
+        this.x = x
+        this.y = y
+    }
 }
 
 class Mokepon {
@@ -33,7 +38,7 @@ app.get("/unirse", (req, res) => {
     res.send(id)
 })
 
-app.post("/mokepon/:jugadorId", (req, res) => {
+app.post("/mokepon/:jugadorId", (req,res) => {
     const jugadorId = req.params.jugadorId || ""
     const nombre = req.body.mokepon || ""
     const mokepon = new Mokepon(nombre)
@@ -45,6 +50,19 @@ app.post("/mokepon/:jugadorId", (req, res) => {
     }
 
     console.log(jugadores)
+    res.end()
+})
+
+app.post("/mokepon/:jugadorId/posicion", (req,res) => {
+    const jugadorId = req.params.jugadorId || ""
+    const x = req.body.x || 0
+    const y = req.body.y || 0
+
+    const jugadorIndex = jugadores.findIndex((jugador) => jugadorId === jugador.id)
+
+    if (jugadorIndex >= 0) {
+        jugadores[jugadorIndex].actualizarPosicion(x,y)
+    }
     res.end()
 })
 
